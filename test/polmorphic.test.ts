7ae76fb757181cf237322c04eec6bca8041b6581
Cory Etzkorn <cory@makenotion.com>
2022-02-01 23:11:23 -0600

    First test working

--- test/polmorphic.test.ts
+++ test/polmorphic.test.ts
@@ -1,25 +1,30 @@
-import { getAssetExtensionFromUrl } from '../src/polymorphicHelpers';
+import { getAssetExtensionFromUrl } from '../src/polymorphicHelpers'
 
 describe('Asset file extension detected', () => {
   it('supports jpg', () => {
-    expect(getAssetExtensionFromUrl('image.jpg')).toEqual('jpg');
-  });
+    expect(getAssetExtensionFromUrl('image.jpg')).toEqual('jpg')
+  })
   it('supports png', () => {
-    expect(getAssetExtensionFromUrl('image.png')).toEqual('png');
-  });
+    expect(getAssetExtensionFromUrl('image.png')).toEqual('png')
+  })
   it('supports gif', () => {
-    expect(getAssetExtensionFromUrl('image.gif')).toEqual('gif');
-  });
+    expect(getAssetExtensionFromUrl('image.gif')).toEqual('gif')
+  })
   it('supports mov', () => {
-    expect(getAssetExtensionFromUrl('video.mov')).toEqual('mov');
-  });
+    expect(getAssetExtensionFromUrl('video.mov')).toEqual('mov')
+  })
   it('supports mp4', () => {
-    expect(getAssetExtensionFromUrl('video.mp4')).toEqual('mp4');
-  });
+    expect(getAssetExtensionFromUrl('video.mp4')).toEqual('mp4')
+  })
   it('supports assets with multiple extensions', () => {
-    expect(getAssetExtensionFromUrl('image.oops.jpg')).toEqual('jpg');
-  });
+    expect(getAssetExtensionFromUrl('image.oops.jpg')).toEqual('jpg')
+  })
   it('supports assets with paths', () => {
-    expect(getAssetExtensionFromUrl('path/image.jpg')).toEqual('jpg');
-  });
-});
+    expect(getAssetExtensionFromUrl('path/image.jpg')).toEqual('jpg')
+  })
+  it('throws error when no file extension present', () => {
+    expect(() => {
+      getAssetExtensionFromUrl('image')
+    }).toThrow('Asset does not have a file extension')
+  })
+})
